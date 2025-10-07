b8be3d39fce36943f21be356eff1dfdee37283b2
FelpaSavage <lipepaz23@gmail.com>
2025-10-06 21:22:36 -0300

    feat(FolhaDePagamento): implementa atributos da entidade, incluindo PK e FK com cardinalidade e totalidade

--- calculo-folha-pagamento/src/main/java/br/pucminas/lab1/grupo6/folha/domain/folha/FolhaDePagamento.java
+++ calculo-folha-pagamento/src/main/java/br/pucminas/lab1/grupo6/folha/domain/folha/FolhaDePagamento.java
@@ -9,4 +9,21 @@ import jakarta.persistence.*;
 @Table (name = "FolhaDePagamento") //Anotação que define o nome da tabela no banco de dados
 public class FolhaDePagamento{
 
+    @Id //Anotação que define o atributo como Primary Key (PK)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE) //Anotação que define que a Primary Key será gerada automaticamente como UUID
+    private int idFolha;
+
+    private YearMonth mes;
+    private Double valorDeDescontoINSS;
+    private Double valorDeDescontoVT;
+    private Double valorDeDescontoVA;
+    private Double valorDeDescontoFGTS;
+    private Double valorDeDescontoIRRF;
+    private Double salarioLiquido;
+    private int diasTrabalhados;
+    private int horasTrabalhadas;
+
+    @ManyToOne (optional = true) //Anotação que aplica cardinalidade N:1 sem obrigatoriedade (optional = true)
+    @JoinColumn (name = "funcionario_id") //Anotação que define idFuncionario como Foreign Key (FK)
+    private Funcionario funcionario;
 }
