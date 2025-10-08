3dfcdfd57c7764334b8691f02f418257c5559c53
Lorena de Paula <lorenaeinfolk@gmail.com>
2025-10-08 09:28:42 -0300

    services

---
+++ calculo-folha-pagamento/src/main/java/br/pucminas/lab1/grupo6/folha/service/FolhaDePagamentoService.java
@@ -0,0 +1,30 @@
+package br.pucminas.lab1.grupo6.folha.service;
+
+import org.springframework.stereotype.Service;
+
+import br.pucminas.lab1.grupo6.folha.domain.folha.FolhaDePagamento;
+import br.pucminas.lab1.grupo6.folha.domain.folha.FolhaRequest;
+import br.pucminas.lab1.grupo6.folha.domain.funcionário.Funcionario;
+
+@Service
+public class FolhaDePagamentoService {
+    public FolhaDePagamento gerarFolhaDePagamento(Funcionario funcionario, FolhaRequest request) {
+        var salarioBruto = funcionario.getSalarioBruto() == null ? 0.0 :funcionario.getSalarioBruto();
+        var descontoInss = switch (funcionario.getCargo()) {
+            case "Estagiário" -> salarioBruto * 0.12;
+            case "Analista" -> salarioBruto * 0.15;
+            case "Gerente" -> salarioBruto * 0.18;
+            case "Diretor" -> salarioBruto * 0.20;
+            default -> salarioBruto * 0.10;
+        };
+        int horasTrabalhadas = (int) Math.round(request.getCargaDiaria() * request.getDiasTrabalhados());
+        var folhaDePagamento = new FolhaDePagamento();
+        folhaDePagamento.setFuncionario(funcionario);
+        folhaDePagamento.setMes(request.getMes());
+        folhaDePagamento.setHorasTrabalhadas(horasTrabalhadas);
+        folhaDePagamento.setDiasTrabalhados(request.getDiasTrabalhados());
+        folhaDePagamento.setValorDeDescontoINSS(descontoInss);
+
+        return folhaDePagamento;
+}
+}
---
+++ calculo-folha-pagamento/src/main/java/br/pucminas/lab1/grupo6/folha/service/SalarioService.java
@@ -0,0 +1,26 @@
+package br.pucminas.lab1.grupo6.folha.service;
+
+import org.springframework.stereotype.Service;
+
+import br.pucminas.lab1.grupo6.folha.domain.folha.FolhaRequest;
+import br.pucminas.lab1.grupo6.folha.domain.funcionário.Funcionario;
+
+@Service
+public class SalarioService {
+    public double calcularSalarioPorHora(Funcionario funcionario, FolhaRequest request) {
+         double salarioBruto = funcionario.getSalarioBruto() == null ? 0.0 : funcionario.getSalarioBruto();
+         double horasTrabalhadasPorMes = request.getJornadaMensal();
+
+         if(horasTrabalhadasPorMes <= 0) {
+              double cargaDiaria = request.getCargaDiaria();
+                int diasTrabalhados = request.getDiasTrabalhados();
+                if(cargaDiaria > 0 && diasTrabalhados > 0) {
+                    horasTrabalhadasPorMes = cargaDiaria * diasTrabalhados;
+                } else {
+                throw new IllegalArgumentException("Jornada mensal não pode ser zero.");
+           }
+        }
+        double valorHora = salarioBruto / horasTrabalhadasPorMes;
+        return Math.round(valorHora * 100.0) / 100.0;
+    }
+}
