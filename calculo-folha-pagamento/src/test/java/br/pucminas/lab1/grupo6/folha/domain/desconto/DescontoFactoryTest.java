fdf844232a8a6b47811257d628b6a4bf80ac6323
Vitor de Roma <vitorroma14@gmail.com>
2025-11-06 09:47:15 -0300

    test(DescontoFactory): garante criação de VA, VT, INSS, FGTS e IRRF

---
+++ calculo-folha-pagamento/src/test/java/br/pucminas/lab1/grupo6/folha/domain/desconto/DescontoFactoryTest.java
@@ -0,0 +1,47 @@
+package br.pucminas.lab1.grupo6.folha.domain.desconto;
+
+import java.time.YearMonth;
+
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
+
+import br.pucminas.lab1.grupo6.folha.domain.folha.FolhaRequest;
+import br.pucminas.lab1.grupo6.folha.domain.funcionário.Funcionario;
+
+class DescontoFactoryTest {
+
+    @Test
+    void deveCriarTiposDeDescontoCorretos() {
+        var factory = new DescontoFactory();
+        var funcionario = new Funcionario();
+        funcionario.setSalarioBruto(1000.0);
+
+        var req = new FolhaRequest();
+        req.setMes(YearMonth.of(2025, 1));
+        req.setDiasTrabalhados(20);
+        req.setCargaDiaria(8);
+        req.setValorValeAlimentacaoDiario(10.0);
+        req.setValeTransporteRecebido(150.0);
+        req.setNumeroDeDependentes(0);
+        req.setValorPensaoAlimenticia(0.0);
+
+        var va  = factory.criarValeAlimentacao(funcionario, req);
+        var vt = factory.criarValeTransporte(funcionario, req);
+        var inss = factory.criarInss(funcionario, req);
+        var fgts = factory.criarFgts(funcionario, req);
+        var irrf = factory.criarIrrf(funcionario, req);
+
+        assertNotNull(va);
+        assertNotNull(vt);
+        assertNotNull(inss);
+        assertNotNull(fgts);
+        assertNotNull(irrf);
+
+        assertInstanceOf(ValeAlimentacao.class, va);
+        assertInstanceOf(ValeTransporte.class, vt);
+        assertInstanceOf(Inss.class, inss);
+        assertInstanceOf(Fgts.class, fgts);
+        assertInstanceOf(Irrf.class, irrf);
+    }
+}
